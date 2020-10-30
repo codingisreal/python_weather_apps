a1477ad26697cbfd74bd87365f0b6e594e04dd88
Gaurav Wairagade <gwairagade@huronconsultinggroup.com>
2020-10-30 18:43:59 +0530

    code change: api errors fixed to hit https requests

--- sandbox/app.py
+++ sandbox/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,4 +14,6 @@ def say_hello():
 @app.route('/weather/<cityname>')
 def show_user(cityname):
   #returns the username
-  return 'CityName: %s' % cityname
+  #return 'CityName: %s' % cityname
+  res=requests.get('http://api.openweathermap.org/data/2.5/weather?'+cityname+'&APPID=9c90d26c16c77d8b3cec65059d078841&units=metric');
+  return res.json();
--- sandbox/venv/Scripts/activate.bat
+++ sandbox/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Development\Python\code\sandbox\venv
+set VIRTUAL_ENV=C:\Users\gwairagade\github\python_weather_apps\sandbox\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
